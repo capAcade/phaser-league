9aba54d29a82029617e855af32ffa75e419a90e8
Eduard Wu <eduard.wu@nl.abnamro.com>
2018-04-16 20:20:54 +0200

    added server

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+// Static serve options
+var options = {
+    index: 'index.html'
+};
+
+// Static serve dist directory on root
+app.use('/', express.static('dist', options));
+
+app.get('/hello', function (req, res) {
+    res.send('Hello World');
+});
+
+app.listen(3000);
