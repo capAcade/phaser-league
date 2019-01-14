05076194f6cfd23f8d751a853b083964490ebc96
Bart Altena <bart.altena@capgemini.com>
2019-01-14 20:12:11 +0100

    Added a max velocity for the ball, to prevent it from going through the wall

--- src/states/Game.js
+++ src/states/Game.js
@@ -458,6 +458,23 @@ Game.prototype = {
         } else {
             self.cars[i].body.angularVelocity = 0;
         }
+
+    },
+    constrainBallVelocity : function (sprite, maxVelocity) {
+        if (!sprite || !sprite.body) {return;}    
+        var body = sprite.body    
+        var angle, currVelocitySqr, vx, vy;    
+        vx = body.data.velocity[0];    
+        vy = body.data.velocity[1];    
+        currVelocitySqr = vx * vx + vy * vy;    
+        if (currVelocitySqr > maxVelocity * maxVelocity) {        
+            angle = Math.atan2(vy, vx);        
+            vx = Math.cos(angle) * maxVelocity;        
+            vy = Math.sin(angle) * maxVelocity;        
+            body.data.velocity[0] = vx;        
+            body.data.velocity[1] = vy;
+        }
+        console.log("BallVelocity: ",currVelocitySqr);
     },
     update: function () {
     var self = this;
@@ -476,5 +493,7 @@ Game.prototype = {
             self.velocity[0] = 0;
             self.velocity[1] = 0;
         }
-    },
+        self.constrainBallVelocity(self.ball, 1024);
+
+    }
 };
